2af0be560783344409abbe13d76af1c1a1b03196
zesciati <zakyabiyyu2003@gmail.com>
2026-09-02 20:39:52 +0700

    erasing pdcv

--- src/pages/Identity.tsx
+++ src/pages/Identity.tsx
@@ -1,6 +1,5 @@
 import "../index.css";
 import FaceZaky from "../assets/pixelimage.png";
-import PdfCvZaky from "../assets/_CV-ZAKY (Updated  2).pdf";
 import CoreSkill from "../components/CoreSkill";
 import Experience from "../components/Experience";
 import Projects from "../components/Projects";
